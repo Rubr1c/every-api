1aabc65213957aac0a56711797d9024a9c94f800
Rubr1c <alizaghloul64@gmail.com>
2025-05-25 17:31:23 +0300

    feat(user): add get user by discord id
    made discord id unique

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -12,15 +12,29 @@ export async function createUser(data: CreateUserInput): Promise<void> {
 }
 
 export async function getUserById(id: bigint): Promise<UserDTO> {
-    const user = await prisma.user.findUnique({
-        where: {
-            id
-        }
-    });
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
 
-    if (!user) {
-        throw new AppError("User not found", "NOT_FOUND");
-    }
+  if (!user) {
+    throw new AppError("User not found", "NOT_FOUND");
+  }
 
-    return userDTO.parse(user); 
+  return userDTO.parse(user);
+}
+
+export async function getUserByDiscordId(id: bigint): Promise<UserDTO> {
+  const user = await prisma.user.findUnique({
+    where: {
+      discordId: id,
+    },
+  });
+
+  if (!user) {
+    throw new AppError("User not found", "NOT_FOUND");
+  }
+
+  return userDTO.parse(user);
 }
